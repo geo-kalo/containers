728ea76427ebda48a9663c0dd854f494c1e71d61
gkalogeropoulos <george.kalogeropoulos@navarino.gr>
2024-09-10 15:07:10 +0300

    add

--- firewall/webserver-api-entrypoint.sh
+++ firewall/webserver-api-entrypoint.sh
@@ -4,6 +4,8 @@ a2enmod ssl
 openssl req -new -newkey rsa:4096 -days 365 -nodes -x509  -subj "/C=GR/ST=George/L=Kalogeropoulos/O=Dis/CN=www.fastapi.com" -keyout /etc/ssl/private/apache.key -out /etc/ssl/certs/apache.crt
 
 mkdir -p /var/www/fastapi/doc
+touch /var/www/fastapi/doc/iptables
+chmod 777 /var/www/fastapi/doc/iptables
 mv /main.py /var/www/fastapi/doc/
 
 /activate_virtual_env.sh
