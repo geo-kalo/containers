f6bfcd7954abd8393c5347621389c17f1023be80
gkalogeropoulos <gkalogeropoulos@navarino.gr>
2024-09-11 14:53:14 +0300

    fix

--- firewall/entrypoint.sh
+++ firewall/entrypoint.sh
@@ -5,10 +5,12 @@
 echo 'nameserver 8.8.8.8' >> /etc/resolv.conf
 iptables-restore < /iptables-rules
 
+
+default_interface=$(route -n | grep 10.0.48 | awk '{print $8}')
 while true; do
 	default_route=$(ip route show default | awk '{print $3}')
 	if [ "$default_route" != "10.0.48.1" ]; then
-		ip route replace default via 10.0.48.1 dev eth0
+		ip route replace default via 10.0.48.1 dev $default_interface
 	fi
 	sleep 10
 done
